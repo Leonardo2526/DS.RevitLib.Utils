cb8fda5a9bd2857ff9fd3a0137dbefdf8bf5a3b1
Leonardo2526 <leonardo147wot@gmail.com>
2022-02-28 21:00:34 +0300

    SolidByCollector added

---
+++ DS.Revit.Utils/ElementFilterUtils.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Revit.Utils
+{
+    public static class ElementFilterUtils
+    {
+        public static ExclusionFilter GetExclustionFilter(List<Element> elements)
+        {
+            ICollection<ElementId> elementIds = elements.Select(el => el.Id).ToList();
+            return new ExclusionFilter(elementIds);
+        }
+    }
+}
---
+++ DS.Revit.Utils/SolidByCollector.cs
@@ -0,0 +1,62 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+namespace DS.Revit.Utils
+{
+    public class SolidByCollector
+    {
+        private FilteredElementCollector Collector;
+        private readonly BoundingBoxIntersectsFilter _boundingBoxFilter;
+        private readonly ExclusionFilter _exclusionFilter;
+
+        //Get solids by collector and filters. Filters are optional.
+        public SolidByCollector(FilteredElementCollector collector,
+            BoundingBoxIntersectsFilter boundingBoxFilter = null,
+            ExclusionFilter exclusionFilter = null)
+        {
+            Collector = collector;
+            _boundingBoxFilter = boundingBoxFilter;
+            _exclusionFilter = exclusionFilter;
+        }
+
+
+        /// <summary>
+        /// Get solids of elements by boundig box from current model.
+        /// </summary>   
+        public Dictionary<Element, List<Solid>> GetModelSolids()
+        {
+            IList<Element> elements = GetElements();
+
+            Dictionary<Element, List<Solid>> collectorSolids = new Dictionary<Element, List<Solid>>();
+
+            foreach (Element elem in elements)
+            {
+                List<Solid> solids = ElementUtils.GetSolids(elem);
+                collectorSolids.Add(elem, solids);
+            }
+
+            return collectorSolids;
+        }
+
+        /// <summary>
+        /// Get elements for solids search
+        /// </summary>
+        /// <returns></returns>
+        private IList<Element> GetElements()
+        {
+            //Get elements by bounding box
+            if (_boundingBoxFilter != null)
+            {
+                Collector.WherePasses(_boundingBoxFilter);
+            }
+
+            //Get elements by exclusions
+            if (_exclusionFilter != null)
+            {
+                Collector.WherePasses(_exclusionFilter);
+            }
+
+            return Collector.ToElements();
+        }
+    }
+}
