b985f7427d93253c7a7f6722eb91d3630555fcd0
Leonardo2526 <leonardo147wot@gmail.com>
2022-03-03 13:25:50 +0300

    MEPCurveUtils added

---
+++ DS.Revit.Utils/MEP/MEPCurveUtils.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Revit.Utils.MEP
+{
+    public static class MEPCurveUtils
+    {
+        public static double GetLength(MEPCurve mEPCurve)
+        {
+            LocationCurve lc = mEPCurve.Location as LocationCurve;
+            return lc.Curve.ApproximateLength;
+        }
+    }
+}
