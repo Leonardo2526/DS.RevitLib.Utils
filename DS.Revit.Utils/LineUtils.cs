9c1d71d1db722ad537c02e029349eb68da239413
Leonardo2526 <leonardo147wot@gmail.com>
2022-03-11 17:05:08 +0300

    CreateModelCurveByPoints added

--- DS.Revit.Utils/LineUtils.cs
+++ DS.Revit.Utils/LineUtils.cs
@@ -1,14 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
 
 namespace DS.Revit.Utils.MEP
 {
-    public class LineUtils
+    public static class LineUtils
     {
 
+        public static ModelCurve CreateModelCurveByPoints(XYZ startPoint, XYZ endPoint, Document doc)
+        {
+            ModelCurve line = null;
+
+            Line geomLine = Line.CreateBound(startPoint, endPoint);
+
+            // Create a geometry plane in Revit application
+            XYZ p1 = startPoint;
+            XYZ p2 = endPoint;
+            XYZ p3 = new XYZ();
+
+            if (Math.Abs(p1.X - p2.X) < 0.01 & Math.Abs(p1.Y - p2.Y) < 0.01)
+                p3 = p2 + XYZ.BasisY;
+            else
+                p3 = p2 + XYZ.BasisZ;
+            Plane geomPlane = Plane.CreateByThreePoints(p1, p2, p3);
+
+            using (Transaction transNew = new Transaction(doc, "DS.CreateModelCurve"))
+            {
+                try
+                {
+                    transNew.Start();
+
+                    // Create a sketch plane in current document
+                    SketchPlane sketch = SketchPlane.Create(doc, geomPlane);
+
+                    // Create a ModelLine element using the created geometry line and sketch plane
+                    line = doc.Create.NewModelCurve(geomLine, sketch) as ModelCurve;
+                }
+
+                catch (Exception e)
+                {
+                    transNew.RollBack();
+                    TaskDialog.Show("Revit", e.ToString());
+                }
+
+                transNew.Commit();
+            }
+            return line;
+        }
 
     }
 }
