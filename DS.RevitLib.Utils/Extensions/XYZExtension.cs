68b0750b73c4c479552df17d59ac292c3db2b22b
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-01 14:43:09 +0300

    DistanceToByBase updated

---
+++ DS.RevitLib.Utils/Extensions/XYZExtension.cs
@@ -0,0 +1,88 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using DS.MainUtils;
+using System;
+using System.Linq;
+using Ivanov.RevitLib.Utils;
+
+namespace DS.RevitLib.Utils.Extensions
+{
+   
+    public static class XYZExtension
+
+    {
+        /// <summary>
+        /// Get XYZ coordinate which value is not zero.
+        /// </summary>
+        /// <param name="xyz"></param>
+        /// <returns>Returns one of not zero coordinate. Throw exception if multiple not zero values exist.</returns>
+        public static int GetNotZeroCoordinate(this XYZ xyz)
+        {
+            var coordinates = new List<int>();
+
+            int prc = 0;
+            int coordNorm;
+
+            coordNorm = xyz.X.Normalize();
+            if (coordNorm.CompareTo(prc) > 0)
+            {
+                coordinates.Add((int)Math.Round(xyz.X));
+            }
+            coordNorm = xyz.Y.Normalize();
+            if (coordNorm.CompareTo(prc) > 0)
+            {
+                coordinates.Add((int)Math.Round(xyz.Y));
+            }
+            coordNorm = xyz.Z.Normalize();
+            if (coordNorm.CompareTo(prc) > 0)
+            {
+                coordinates.Add((int)Math.Round(xyz.Z));
+            }
+
+            if (coordinates.Count > 1)
+            {
+                throw new InvalidOperationException("XYZ has multiple not zero values.");
+            }
+
+            return coordinates.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get distance between two point by reference base point.
+        /// </summary>
+        /// <param name="basePoint"></param>
+        /// <param name="startPoint"></param>
+        /// <param name="endPoint"></param>
+        /// <returns>Returns positive value if startPoint is between base point and endPoint. 
+        /// Returns negative value if endPoint is between base point and startPoint</returns>
+        public static double DistanceToByBase(this XYZ startPoint, XYZ basePoint, XYZ endPoint)
+        {
+            double distBase_Start = basePoint.DistanceTo(startPoint);
+            double distBase_End = basePoint.DistanceTo(endPoint);
+
+            XYZ vectorBase1 = (basePoint - startPoint).AbsXYZ();
+            XYZ vectorBase2 = (basePoint - endPoint).AbsXYZ();
+
+            double angleRad = vectorBase1.AngleTo(vectorBase2);
+            double angleDeg = angleRad * 180 / Math.PI;
+
+            int normCoordStart = GetNormCoordinate(startPoint, basePoint, angleRad);
+            int normCoordEnd = GetNormCoordinate(endPoint, basePoint, angleRad);
+
+            double distance = (distBase_End * normCoordEnd - distBase_Start * normCoordStart) * normCoordEnd;
+
+            return distance;
+        }
+
+        private static int GetNormCoordinate(XYZ p1, XYZ p2, double angleRad)
+        {
+            XYZ vector = p1 - p2;
+
+            vector = new XYZ(vector.X * Math.Sin(angleRad), vector.X * Math.Cos(angleRad), vector.Z);
+
+            XYZ vectorNorm = vector.Normalize();
+
+            return vectorNorm.GetNotZeroCoordinate();
+        }
+    }
+}
