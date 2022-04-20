dcda3970dd6cd83b0111283ef0d0e4cdf0d396b1
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-20 17:52:31 +0300

    MEPSystemBuilder added

---
+++ DS.RevitLib.Utils/MEP/Creator/Builders/BuilderByPoints.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.RevitLib.Utils.MEP.Creator
+{
+    public class BuilderByPoints : MEPSystemBuilder
+    {
+        public BuilderByPoints(MEPCurve baseMEPCurve, List<XYZ> points) :base(baseMEPCurve)
+        {
+            this._Points = points;
+        }
+
+        private List<XYZ> _Points = new List<XYZ>();
+
+        public override MEPCurvesModel BuildMEPCurves()
+        {
+            MEPCurveCreator mEPCurveCreator = new MEPCurveCreator(Doc, BaseMEPCurve);
+
+            for (int i = 0; i < _Points.Count - 1; i++)
+            {
+                XYZ p1 = _Points[i];
+                XYZ p2 = _Points[i + 1];
+
+                MEPCurve mEPCurve = mEPCurveCreator.CreateMEPCurveByPoints(p1, p2);
+                MEPSystemModel.AllElements.Add(mEPCurve);
+                MEPSystemModel.MEPCurves.Add(mEPCurve);
+            }
+
+            GetConnectors();
+
+            return new MEPCurvesModel(MEPSystemModel, Doc);
+        }
+    }
+}
---
+++ DS.RevitLib.Utils/MEP/Creator/MEPSystemModel.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.RevitLib.Utils.MEP.Creator
+{
+    public class MEPSystemModel
+    {
+
+        #region Properties
+
+        public List<Element> MEPCurves { get; set; } = new List<Element>();
+        public List<Element> AllElements { get; set; } = new List<Element>();
+
+        public Connector StartConnector { get; set; }
+        public Connector EndConnector { get; set; }
+
+        #endregion
+    }
+}
