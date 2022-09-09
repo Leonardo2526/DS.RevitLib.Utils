3519158733b1e2eb80d89b83151d8f6f2219c42f
Leonardo2526 <leonardo147wot@gmail.com>
2022-09-09 22:21:41 +0300

    SolidCollisionChecker updated

--- DS.RevitLib.Utils/Collisions/Checkers/SolidCollisionChecker.cs
+++ DS.RevitLib.Utils/Collisions/Checkers/SolidCollisionChecker.cs
@@ -16,7 +16,7 @@ namespace DS.RevitLib.Utils.Collisions.Checkers
         {
         }
 
-        public SolidCollisionChecker(List<SolidModelExt> checkedObjects1, List<Element> checkedObjects2, List<Element> exludedObjects = null) : 
+        public SolidCollisionChecker(List<SolidModelExt> checkedObjects1, List<Element> checkedObjects2, List<Element> exludedObjects = null) :
             base(checkedObjects1, checkedObjects2, exludedObjects)
         {
         }
@@ -40,29 +40,33 @@ namespace DS.RevitLib.Utils.Collisions.Checkers
             }
         }
 
+            public List<ICollision> AllCollisions { get; private set; } = new List<ICollision>();
 
         private List<ICollision> GetObjectCollisions(SolidModelExt object1)
         {
-            List<Element> elements;
+            var excludedElementsIds = new List<ElementId>();
 
-            if (ExclusionFilter is null)
+            if (ExludedObjects is not null && ExludedObjects.Any())
             {
-                elements = Collector.WherePasses(new ElementIntersectsSolidFilter(object1.Solid)).
-                    ToElements().ToList();
+                excludedElementsIds.AddRange(ExludedObjects.Select(obj => obj.Id).ToList());
             }
-            else
-            {
-                elements = Collector.WherePasses(new ElementIntersectsSolidFilter(object1.Solid)).
-                    WherePasses(ExclusionFilter).
-                    ToElements().ToList();
-            }
+
+            excludedElementsIds.Add(object1.Element.Id);
+            var exculdedFilter = new ExclusionFilter(excludedElementsIds);
+
+            List<Element>  elements = Collector.WherePasses(new ElementIntersectsSolidFilter(object1.Solid)).
+                WherePasses(exculdedFilter).
+                ToElements().ToList();
 
             //build all object1 collisions
             var collisions = new List<ICollision>();
             foreach (var elem in elements)
             {
                 var col = BuildCollision(object1, elem);
-                collisions.Add(col);
+                if(!CollisionExist(AllCollisions, col))
+                {
+                    collisions.Add(col);
+                }
             }
 
             return collisions;
@@ -75,19 +79,18 @@ namespace DS.RevitLib.Utils.Collisions.Checkers
                 return null;
             }
 
-            var allCollisions = new List<ICollision>();
 
-            foreach (var item in CheckedObjects1)
+            foreach (SolidModelExt object1 in CheckedObjects1)
             {
-                var collisions = GetObjectCollisions(item);
+                var collisions = GetObjectCollisions(object1);
                 if (collisions is null || !collisions.Any())
                 {
                     continue;
                 }
-                allCollisions.AddRange(collisions);
+                AllCollisions.AddRange(collisions);
             }
 
-            return allCollisions;
+            return AllCollisions;
         }
 
         protected override ICollision BuildCollision(SolidModelExt object1, Element object2)
@@ -100,6 +103,20 @@ namespace DS.RevitLib.Utils.Collisions.Checkers
             CheckedObjects1 = checkedObjects1;
             return GetCollisions();
 
+        }
+
+        public bool CollisionExist(List<ICollision> collisions, ICollision collision)
+        {
+            var sCollision = collision as SolidElemCollision;
+            foreach (SolidElemCollision existCollison in collisions.Cast<SolidElemCollision>())
+            {
+                if (sCollision.Object1.Element.Id == existCollison.Object2.Id &&
+                    existCollison.Object1.Element.Id == sCollision.Object2.Id)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
