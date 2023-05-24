a35db73d1d250a9c86590feb02399b0d30bbe4c2
Данил Сараев <leonardo147wot@gmail.com>
2023-05-24 18:35:52 +0300

    iterator fixed

--- DS.RevitLib.Utils/Collisions/Solutions/ISolution.cs
+++ DS.RevitLib.Utils/Collisions/Solutions/ISolution.cs
@@ -1,9 +1,18 @@
-﻿namespace DS.RevitLib.Utils.Collisions.Solutions
+﻿using DS.RevitLib.Utils.Transactions;
+using System.Threading.Tasks;
+
+namespace DS.RevitLib.Utils.Collisions.Solutions
 {
     /// <summary>
     /// An object that represents collision solution.
     /// </summary>
     public interface ISolution
     {
+        /// <summary>
+        /// Show current solution.
+        /// </summary>
+        /// <param name="trb"></param>
+        /// <returns></returns>
+        public Task ShowAsync(AbstractTransactionBuilder trb);
     }
 }
