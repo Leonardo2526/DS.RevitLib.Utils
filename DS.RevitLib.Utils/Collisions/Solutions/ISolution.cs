e965aceab440492bb621085892eff54fb2a27577
Данил Сараев <leonardo147wot@gmail.com>
2023-05-27 19:09:28 +0300

    ISolution upd

--- DS.RevitLib.Utils/Collisions/Solutions/ISolution.cs
+++ DS.RevitLib.Utils/Collisions/Solutions/ISolution.cs
@@ -4,15 +4,10 @@ using System.Threading.Tasks;
 namespace DS.RevitLib.Utils.Collisions.Solutions
 {
     /// <summary>
-    /// An object that represents collision solution.
+    /// An object that represents solution.
     /// </summary>
     public interface ISolution
     {
-        /// <summary>
-        /// Show current solution.
-        /// </summary>
-        /// <param name="trb"></param>
-        /// <returns></returns>
-        public Task ShowAsync(AbstractTransactionBuilder trb);
+     
     }
 }
